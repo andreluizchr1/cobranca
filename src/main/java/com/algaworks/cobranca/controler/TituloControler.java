1556af0045fd8aed8fd999fc86199f39dcf9321d
André Luiz Chaves Rodrigues <andreluizchr1@gmail.com>
2018-01-02 18:33:06 -0300

    adicionando mensagem de sucesso com o thymeleaf na página de cadastro de titulos

--- src/main/java/com/algaworks/cobranca/controler/TituloControler.java
+++ src/main/java/com/algaworks/cobranca/controler/TituloControler.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
@@ -21,9 +22,11 @@ public class TituloControler {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
+	public ModelAndView salvar(Titulo titulo) {
 		System.out.println(">>>>" + titulo.getDescricao());
 		titulos.save(titulo);
-		return "CadastroTitulo";
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Titulo salvo com sucesso!");
+		return mv;
 	}
 }
