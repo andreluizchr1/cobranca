0ae8d329b05cad5eeb4a45415be8e0582f77962c
André Luiz Chaves Rodrigues <andreluizchr1@gmail.com>
2018-01-05 11:52:01 -0300

    alteração das paginas CadastroTitulo.html e PesquisaTitulos e TituloController para possibilitar a edição

--- src/main/java/com/algaworks/cobranca/controler/TituloControler.java
+++ src/main/java/com/algaworks/cobranca/controler/TituloControler.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,18 +25,28 @@ public class TituloControler {
 	@Autowired
 	private Titulos titulos;
 
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
 		return mv;
 	}
 
 	@RequestMapping
 	public ModelAndView pesquisar() {
-		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		
 		List<Titulo> todosTitulos = titulos.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
+		return mv;
+	}
+
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
 		return mv;
 	}
 
@@ -43,13 +54,13 @@ public class TituloControler {
 	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 
 		if (errors.hasErrors()) {
-			return "CadastroTitulo";
+			return CADASTRO_VIEW;
 		}
-		
+
 		titulos.save(titulo);
 		attributes.addFlashAttribute("mensagem", "Titulo salvo com sucesso!");
 		return "redirect:/titulos/novo";
-		
+
 	}
 
 	@ModelAttribute("todosStatusTitulo")
