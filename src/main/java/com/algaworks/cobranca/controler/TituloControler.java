9b37b5b106f1284b266410dd5b4fa6d30b734883
André Luiz Chaves Rodrigues <andreluizchr1@gmail.com>
2018-01-08 20:52:36 -0300

    Recebendo titulo com ajax parte 1

--- src/main/java/com/algaworks/cobranca/controler/TituloControler.java
+++ src/main/java/com/algaworks/cobranca/controler/TituloControler.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -88,4 +87,5 @@ public class TituloControler {
 		attributes.addFlashAttribute("mensagem", "Título excluído com sucesso");
 		return "redirect:/titulos";
 	}
+	
 }
--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -28,6 +28,13 @@ $('#confirmacaoExclusaoModal').on(
 $(function(){
 	$('[rel="tooltip"]').tooltip();
 	$('.js-currency').maskMoney({decimal:',', thousands:'.', allowZero:true});
+	
+	$('.js-atualizar-status').on('click', function(event){
+		event.preventDefault();
+		var botaoReceber = $(event.currentTarget);
+		var urlReceber = botaoReceber.attr('href');
+		console.log('urlReceber', urlReceber);
+	});
 });
 
 
